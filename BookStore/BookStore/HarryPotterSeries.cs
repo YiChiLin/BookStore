e388c1fa5aff536c20b4389eb7328bb8e1844edb
YiChi Lin <sky61054@gmail.com>
2017-06-20 09:58:53 +0800

    Add first test case

---
+++ BookStore/BookStore/HarryPotterSeries.cs
@@ -0,0 +1,7 @@
+﻿namespace BookStore
+{
+    internal class HarryPotterSeries 
+    {
+
+    }
+}
---
+++ BookStore/BookStoreTests/HarryPotterSeriesTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BookStore.Tests
+{
+    [TestClass()]
+    public class HarryPotterSeriesTests
+    {
+        [TestMethod()]
+        public void Buy_1_HarryPotterBook_Is_NT100()
+        {
+            Assert.Fail();
+        }
+    }
+}
